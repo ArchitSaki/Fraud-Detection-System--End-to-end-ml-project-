ad28836c34ba37e8107302f694430ab72476f9bd
archit <itsarchit.saki@gmail.com>
2026-03-02 15:55:05 +0530

    cicd 8

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -10,14 +10,20 @@ class FlaskAppTests(unittest.TestCase):
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'<title>Fraud Detection</title>', response.data)
+        self.assertIn(b'<title>Fraud Detection System</title>', response.data)
 
     def test_predict_page(self):
-        response = self.client.post('/predict', data=dict(text="I love this!"))
+        response = self.client.post('/predict', data={
+            "type": "TRANSFER",
+            "amount": 1000,
+            "oldbalanceOrg": 5000,
+            "newbalanceOrig": 4000,
+            "oldbalanceDest": 0,
+            "newbalanceDest": 1000
+        })
         self.assertEqual(response.status_code, 200)
         self.assertTrue(
-            b'Fraud' in response.data or b'Legitimate' in response.data,
-            "Response should contain either 'Fraud' or 'Legitimate'"
+            b'Fraud' in response.data or b'Legitimate' in response.data
         )
 
 if __name__ == '__main__':
